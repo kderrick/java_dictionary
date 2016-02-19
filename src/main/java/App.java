b7742725723a7d218bae76eb3e5501db77e01248
Kyle Derrick <kderrick1988@gmail.com>
2016-02-19 13:56:38 -0800

    add get method for / , defintions, definitions/new and post method for /defintitions

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.ArrayList;
 
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
@@ -6,19 +7,39 @@ import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
-
-        ProcessBuilder process = new ProcessBuilder();
-        Integer port;
-        if (process.environment().get("PORT") != null) {
-            port = Integer.parseInt(process.environment().get("PORT"));
-        } else {
-            port = 4567;
-        }
-
-        setPort(port);
-
         staticFileLocation("/public");
         String layout = "templates/layout.vtl";
+
+        get("/", (request, response) -> {
+          HashMap<String,Object>model = new HashMap<String, Object>();
+          model.put("definitions", request.session().attribute("definitions"));
+          model.put("template", "templates/index.vtl");
+          return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+        get("/definitions", (request, response) -> {
+          HashMap<String, Object> model = new HashMap<String, Object>();
+          model.put("definitions", Definition.all());
+          model.put("template", "templates/definitions.vtl");
+          return new ModelAndView(model, layout);
+          }, new VelocityTemplateEngine());
+
+        get("definitions/new", (request, response) -> {
+          HashMap<String, Object> model = new HashMap<String, Object>();
+          model.put("template", "templates/definition-form.vtl");
+          return new ModelAndView(model, layout);
+          }, new VelocityTemplateEngine());
+
+        post("/definitions", (request,response) -> {
+          HashMap<String, Object>model = new HashMap<String, Object>();
+          Word word = Word.find(Integer.parseInt(request.queryParams("wordId")));
+          String description = request.queryParams("description");
+          Definition newDefinition = new Definition(description);
+          word.addDefinition(newDefinition);
+          model.put("word", word);
+          model.put("template", "templates/word.vtl");
+          return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
 
         //RESTful ARCHITECTURE
         //Use POST to create something on the server
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -18,6 +18,10 @@ public class AppTest extends FluentTest {
   @ClassRule
   public static ServerRule server = new ServerRule();
 
+  @Test
+  public void rootTest() {
+      goTo("http://localhost:4567/");
+      assertThat(pageSource()).contains("Dictionary");
+  }
 
-  //Tests go here
 }
