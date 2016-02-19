51077b82722376b1c562645b147f30e03a2df122
Kyle Derrick <kderrick1988@gmail.com>
2016-02-19 08:10:52 -0800

    replace template with correct app info

---
+++ src/test/java/DefinitionTest.java
@@ -0,0 +1,9 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class DefinitionTest {
+
+
+  //Tests go here
+
+}
---
+++ src/test/java/WordTest.java
@@ -0,0 +1,9 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class WordTest {
+
+
+  //Tests go here
+
+}
