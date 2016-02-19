d5218b043a2516c5b6eb8272233382f8b4722581
Kyle Derrick <kderrick1988@gmail.com>
2016-02-19 14:19:08 -0800

    add post method for words

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,39 +7,69 @@ import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
-        staticFileLocation("/public");
-        String layout = "templates/layout.vtl";
+      staticFileLocation("/public");
+      String layout = "templates/layout.vtl";
 
-        get("/", (request, response) -> {
-          HashMap<String,Object>model = new HashMap<String, Object>();
-          model.put("definitions", request.session().attribute("definitions"));
-          model.put("template", "templates/index.vtl");
-          return new ModelAndView(model, layout);
+      get("/", (request, response) -> {
+        HashMap<String,Object>model = new HashMap<String, Object>();
+        model.put("definitions", request.session().attribute("definitions"));
+        model.put("template", "templates/index.vtl");
+        return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
+
+      get("/definitions", (request, response) -> {
+        HashMap<String, Object> model = new HashMap<String, Object>();
+        model.put("definitions", Definition.all());
+        model.put("template", "templates/definitions.vtl");
+        return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
 
-        get("/definitions", (request, response) -> {
-          HashMap<String, Object> model = new HashMap<String, Object>();
-          model.put("definitions", Definition.all());
-          model.put("template", "templates/definitions.vtl");
-          return new ModelAndView(model, layout);
-          }, new VelocityTemplateEngine());
-
-        get("definitions/new", (request, response) -> {
-          HashMap<String, Object> model = new HashMap<String, Object>();
-          model.put("template", "templates/definition-form.vtl");
-          return new ModelAndView(model, layout);
-          }, new VelocityTemplateEngine());
-
-        post("/definitions", (request,response) -> {
-          HashMap<String, Object>model = new HashMap<String, Object>();
-          Word word = Word.find(Integer.parseInt(request.queryParams("wordId")));
-          String description = request.queryParams("description");
-          Definition newDefinition = new Definition(description);
-          word.addDefinition(newDefinition);
-          model.put("word", word);
-          model.put("template", "templates/word.vtl");
-          return new ModelAndView(model, layout);
+      get("definitions/new", (request, response) -> {
+        HashMap<String, Object> model = new HashMap<String, Object>();
+        model.put("template", "templates/definition-form.vtl");
+        return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
+
+      post("/definitions", (request,response) -> {
+        HashMap<String, Object>model = new HashMap<String, Object>();
+        Word word = Word.find(Integer.parseInt(request.queryParams("wordId")));
+        String description = request.queryParams("description");
+        Definition newDefinition = new Definition(description);
+        word.addDefinition(newDefinition);
+        model.put("word", word);
+        model.put("template", "templates/word.vtl");
+        return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
+
+      get("/definitions/:id", (request, response) -> {
+        HashMap<String, Object> model = new HashMap<String, Object>();
+        Definition definition = Definition.find(Integer.parseInt(request.params(":id")));
+        model.put("definition", definition);
+        model.put("template", "templates/definition.vtl");
+        return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
+
+      get("/words", (request, response) -> {
+        HashMap<String, Object> model = new HashMap<String, Object>();
+        model.put("words", Word.all());
+        model.put("template", "templates/words.vtl");
+        return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
+
+      get("words/new", (request, response) -> {
+        HashMap<String, Object> model = new HashMap<String, Object>();
+        model.put("template", "templates/word-form.vtl");
+        return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
+
+      post("/words", (request,response) -> {
+        HashMap<String, Object>model = new HashMap<String, Object>();
+        String name = request.queryParams("name");
+        Word newWord = new Word(name);
+        model.put("word", newWord);
+        model.put("template", "templates/success.vtl");
+        return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
 
         //RESTful ARCHITECTURE
         //Use POST to create something on the server
